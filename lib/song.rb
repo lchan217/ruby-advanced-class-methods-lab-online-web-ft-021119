f5d75650891ea2fdb3731c61bdef4d59554522ad
Lizzie Chan <lchan217@gmail.com>
2019-03-06 15:46:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,6 @@ class Song
     find_by_name(name) || create_by_name(name)
   end 
   def self.alphabetical
-    @@all.sort_by {|a,b| a <=> b}
+    @@all.sort_by {|a,b| a.name <=> b.name}
   end 
  end
