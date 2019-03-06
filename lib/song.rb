a035d87811d07f5dcc9a0a76f1e04e34486ceb97
Lizzie Chan <lchan217@gmail.com>
2019-03-06 15:52:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,53 @@
-require "pry"
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
- 
-  def self.all
-    @@all
+def save
+    self.class.all << self
   end
-  def save 
-    self.class.all << self 
-  end 
+
   def self.create
-    new = self.new 
-    new.save 
-    new 
-  end 
-  def self.new_by_name(song_name)
     new = self.new
-    new.name = song_name
-    new
-  end 
-  def self.create_by_name(song)
-    new = self.new
-    new.name = song
     new.save
-    new 
-  end 
-  def self.find_by_name(name)
-    @@all.find {|song| song.name == name}
+    new
   end
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create_by_name(name)
-  end 
+ 
+  def self.new_by_name(string_name)
+    new = self.new
+    new.name = string_name
+    new
+  end
+ 
+  def self.create_by_name(string_name)
+    new = self.new
+    new.name = string_name
+    new.save
+    new
+  end
+ 
+  def self.find_by_name(string_name)
+    @@all.find {|song| song.name == string_name}
+  end
+ 
+  def self.find_or_create_by_name(string_name)
+    find_by_name(string_name) || create_by_name(string_name)
+  end
+ 
   def self.alphabetical
     @@all.sort_by {|title| title.name}
   end
+ 
   def self.new_from_filename(file)
-    #file = "Thundercat - For Love I Come.mp3"
-    final = file.split(" - ")
-    song = final[1]
-    artist = final[0]
-    
-    new = self.new 
-    new.name = song.chomp(".mp3")
-    new.artist_name = artist
-    new.save
-    new
-  end 
- end
+    remove_end = file.chomp(".mp3")
+    final = remove_end.split(" - ")
+   
+    song = self.new
+    song.name = final [1]
+    song.artist_name = final[0]
+    song
+  end
+ 
+  def self.create_from_filename(file)
+    new_from_filename(file).save
+  end
+ 
+  def self.destroy_all
+    @@all.clear
+  end
+end
